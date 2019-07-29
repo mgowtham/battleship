033cfb8273b61d736ce842dae374f23cd0f14689
gowthammanivannan <gowtham.manivannan@freshworks.com>
2019-07-29 19:42:05 +0530

    code cleanup

--- tests/integration/components/battle-board-test.js
+++ tests/integration/components/battle-board-test.js
@@ -4,38 +4,28 @@ import hbs from 'htmlbars-inline-precompile';
 import { click } from 'ember-native-dom-helpers';
 
 module('Integration | Component | battle-board', function(hooks) {
+  const SHIPS = [{ size: 4 }, { size: 3 }, { size: 2 }, { size: 2 }, { size: 3 }, { size: 3 }];
   setupRenderingTest(hooks);
+  hooks.beforeEach( function() {
+    this.set('ships', SHIPS);
+   });
 
   test('check whether placement test works properly', async function(assert) {
     this.set('testMode', false);
-    let ships = [{ size: 4 }, { size: 3 }, { size: 2 }, { size: 2 }, { size: 3 }, { size: 3 }];
-    this.set('ships', ships);
     await this.render(hbs`{{battle-board testMode=testMode ships=ships}}`);
-    let row, column;
-    if (this.get('ships.firstObject.isHorizontal')) {
-       row = this.get('ships.firstObject.fixedPos');
-       column = this.get('ships.firstObject.variablePos');
-    } else {
-      column = this.get('ships.firstObject.fixedPos');
-      row = this.get('ships.firstObject.variablePos');
-    }
+    let fixedPos = this.get('ships.firstObject.fixedPos'), 
+        variablePos = this.get('ships.firstObject.variablePos');
+    let [row, column] = this.get('ships.firstObject.isHorizontal') ? [fixedPos, variablePos] : [variablePos, fixedPos];
     assert.equal(this.$(`.row:nth-child(${row + 2}) .cell:nth-child(${column + 2})`).text().trim(), 'x');
     assert.equal(this.$(`.row:first-child .cell:nth-child(2)`).text().trim(), 'a');
   });
 
   test('check whether test game works properly', async function(assert) {
     this.set('testMode', true);
-    let ships = [{ size: 4 }, { size: 3 }, { size: 2 }, { size: 2 }, { size: 3 }, { size: 3 }];
-    this.set('ships', ships);
     await this.render(hbs`{{battle-board testMode=testMode ships=ships}}`);
-    let row, column;
-    if (this.get('ships.firstObject.isHorizontal')) {
-       row = this.get('ships.firstObject.fixedPos');
-       column = this.get('ships.firstObject.variablePos');
-    } else {
-      column = this.get('ships.firstObject.fixedPos');
-      row = this.get('ships.firstObject.variablePos');
-    }
+    let fixedPos = this.get('ships.firstObject.fixedPos'), 
+        variablePos = this.get('ships.firstObject.variablePos');
+    let [row, column] = this.get('ships.firstObject.isHorizontal') ? [fixedPos, variablePos] : [variablePos, fixedPos];
     assert.equal(this.$(`.row:nth-child(${row + 2}) .cell:nth-child(${column + 2})`).text().trim(), '');
     await click(`.row:nth-child(${row + 2}) .cell:nth-child(${column + 2})`);
     assert.equal(this.$(`.row:nth-child(${row + 2}) .cell:nth-child(${column + 2})`).text().trim(), 'x');
